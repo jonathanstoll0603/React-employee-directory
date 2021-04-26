05f91ca5ee5707aebf155e1913b458c8bd5a6523
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-04-26 08:26:58 -0400

    added API call, handleInputChange function, and updated render()

--- employee-directory/src/pages/Home.js
+++ employee-directory/src/pages/Home.js
@@ -1,54 +1,64 @@
 import React, { Component } from "react";
-import Header from "../components/Header/Header";
-import Wrapper from "../components/Wrapper/Wrapper";
 import Container from "../components/Container/Container";
 import Row from "../components/Row/Row";
 import Col from "../components/Col/Col";
-import Footer from "../components/Footer/Footer";
 import API from "../utils/API";
 import Search from "../components/Search/Search";
 
-class EmployeeSearch extends Component {
+class Home extends Component {
   state = {
-    result: {},
-    search: ""
+    results: [],
+    employees: [],
+    names: [],
+    dob: [],
+    search: "",
+    error: ""
+  };
+
+  componentDidMount() {
+    API.getEmployees()
+      .then(res => this.setState({ employees: res.data.message }))
+      .catch(err => console.log(err));
+  }
+
+  handleInputChange = event => {
+    this.setState({ search: event.target.value });
   };
 
   render() {
     return (
-      <Header />
-      <Wrapper>
         <Container>
+            <Search />
             <Row>
-            <Col size="md-8">
-                <Card
-                heading={this.state.result.Title || "Search for a Movie to Begin"}
-                >
-                {!this.state.result.Title ? <span>No movie found, sorry!</span> : <MovieDetail
-                    title={this.state.result.Title}
-                    src={this.state.result.Poster}
-                    director={this.state.result.Director}
-                    genre={this.state.result.Genre}
-                    released={this.state.result.Released}
-                />}
-                
-                </Card>
-            </Col>
-            <Col size="md-4">
-                <Card heading="Search">
-                <SearchForm
-                    value={this.state.search}
-                    handleInputChange={this.handleInputChange}
-                    handleFormSubmit={this.handleFormSubmit}
-                />
-                </Card>
-            </Col>
+                <Col size="md-2">
+                    <div>
+                        Testing
+                    </div>
+                </Col>
+                <Col size="md-2">
+                    <div>
+                        Testing2
+                    </div>
+                </Col>
+                <Col size="md-2">
+                    <div>
+                        Testing3
+                    </div>
+                </Col>
+                <Col size="md-2">
+                    <div>
+                        Testing4
+                    </div>
+                </Col>
+                <Col size="md-2">
+                    <div>
+                        Testing5
+                    </div>
+                </Col>
             </Row>
         </Container>
-      </Wrapper>
-      <Footer />
     );
   }
 }
 
-export default OmdbContainer;
+export default Home;
