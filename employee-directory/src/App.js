ec9c916e832350b2eefa6bda4327ae29a46dc880
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-04-26 08:24:59 -0400

    finalized App.js structure

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import Home from "./pages/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Wrapper from "./components/Wrapper/Wrapper";
 
 function App() {
-  return <Home />;
+  return (
+    <div>
+      <Header />
+      <Wrapper>
+        <Home />
+      </Wrapper>
+      <Footer />
+    </div>
+  )
 }
 
 export default App;
