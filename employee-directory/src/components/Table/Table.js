d40ae0e8739d0de15e9db355ccfa292bebcde95c
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-04-29 03:03:34 -0400

    added button to name column along with onClick handler to test sorting by first name

--- employee-directory/src/components/Table/Table.js
+++ employee-directory/src/components/Table/Table.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TableBody from "../TableBody/TableBody";
+import "./Table.css"
 
 function Table(props) {
     return ( 
@@ -7,18 +8,17 @@ function Table(props) {
             <thead>
                 <tr>
                     <th scope="col">Image</th>
-                    <th scope="col">Name</th>
+                    <th scope="col"> Name
+                    <button type="button" className="btn btn-light px-2" onClick={props.handleNameSort}><i className="fas fa-caret-down"></i></button>
+                    </th>
                     <th scope="col">Phone</th>
                     <th scope="col">Email</th>
                     <th scope="col">DOB (YYYY-MM-DD)</th>
                 </tr>
-            </thead>
-
-            {props.results.map((result, index) => (
-            <TableBody key={index} alt={result.name.first + result.name.last} src={result.picture.thumbnail} firstName={result.name.first} lastName={result.name.last} phoneNumber={result.phone} emailAddress={result.email} dob={result.dob.date}/>
-            ))}
-    
-            
+            </thead>  
+                {props.results.map((result, index) => (
+                <TableBody key={index} alt={result.name.first + result.name.last} src={result.picture.thumbnail} firstName={result.name.first} lastName={result.name.last} phoneNumber={result.phone} emailAddress={result.email} dob={result.dob.date}/>
+                ))}
         </table>
     )
 }
