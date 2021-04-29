2e4913c5e78c7f12a573bcea7f0f560f6696709e
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-04-28 21:56:01 -0400

    finished rendering of API info

--- employee-directory/src/pages/Home.js
+++ employee-directory/src/pages/Home.js
@@ -1,61 +1,42 @@
 import React, { Component } from "react";
 import Container from "../components/Container/Container";
-import Row from "../components/Row/Row";
-import Col from "../components/Col/Col";
+import Table from "../components/Table/Table";
+import TableBody from "../components/TableBody/TableBody";
 import API from "../utils/API";
 import Search from "../components/Search/Search";
 
 class Home extends Component {
   state = {
-    results: [],
-    employees: [],
-    names: [],
+    name: [],
     dob: [],
+    phoneNumber: [],
+    email: [],
+    results: [],
     search: "",
     error: ""
   };
 
   componentDidMount() {
     API.getEmployees()
-      .then(res => this.setState({ employees: res.data.message }))
+      .then(res => {
+        this.setState({ results: res.data.results });
+        console.log(this.state.results);
+      })
       .catch(err => console.log(err));
   }
 
   handleInputChange = event => {
     this.setState({ search: event.target.value });
   };
+  
 
   render() {
     return (
         <Container>
             <Search />
-            <Row>
-                <Col size="md-2">
-                    <div>
-                        Testing
-                    </div>
-                </Col>
-                <Col size="md-2">
-                    <div>
-                        Testing2
-                    </div>
-                </Col>
-                <Col size="md-2">
-                    <div>
-                        Testing3
-                    </div>
-                </Col>
-                <Col size="md-2">
-                    <div>
-                        Testing4
-                    </div>
-                </Col>
-                <Col size="md-2">
-                    <div>
-                        Testing5
-                    </div>
-                </Col>
-            </Row>
+            <Table results={this.state.results}>
+              <TableBody />
+            </Table>
         </Container>
     );
   }
